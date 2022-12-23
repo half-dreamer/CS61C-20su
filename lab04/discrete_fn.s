76329257225317443bc0075d6e16d7026a77e3ed
half-dreamer <zy1319097537@gmail.com>
2022-12-23 15:32:17 +0800

    finish lab04

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -77,6 +77,12 @@ main:
 # Think: why might having a1 be useful?
 f:
     # YOUR CODE GOES HERE!
+    addi t0, a0, 3
+    li  t1, 4
+    mul  t0, t1, t0
+    add t2, t0, a1
+    lw a0, 0(t2)  
+    
 
     jr ra               # Always remember to jr ra after your function!
 
