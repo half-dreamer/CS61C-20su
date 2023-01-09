c3473c3b935daf74b62b48e0e2085c5fbe714a9e
half-dreamer <zy1319097537@gmail.com>
2023-01-09 17:52:54 +0800

    buggy version simd

--- lab09/common.c
+++ lab09/common.c
@@ -53,9 +53,26 @@ long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
 	
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* YOUR CODE GOES HERE */
-
+		int sum[4];
+		__m128i sum_vector = _mm_setzero_si128();
+		for (unsigned int i = 0; i + 4 <= NUM_ELEMS ; i += 4) {
+			__m128i cur_vector = _mm_loadu_si128((__m128i *)(vals + i));
+			__m128i mask_vector = _mm_cmpgt_epi32(cur_vector, _127);
+			cur_vector = _mm_and_si128(cur_vector, mask_vector);
+			sum_vector += _mm_add_epi32(sum_vector, cur_vector);	
+		}
+		// can't add like result += sum_vector[0] + ..., because vector can't be operated like an array
+		// so we create an array to store the four value of sum_vector and use that array to add values
+		_mm_storeu_si128((__m128i *)sum, sum_vector); 
+		for (unsigned int i = 0; i < 4; i++) {
+			result += sum[i];
+		}
 		/* You'll need a tail case. */
-
+		for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS;	i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
